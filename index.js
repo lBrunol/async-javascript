11c5d84aa26ee3c1bb81f56937151f6a06824767
Bruno Alves <bruno.ar.avl@gmail.com>
2020-02-05 01:10:19 -0300

    Ajustes visuais

--- index.js
+++
@@ -1,9 +0,0 @@
-function bloqueioRenderizacao(){
-  for(let i = 0; i < 150000; i++){
-    console.log(i);
-  }
-}
-
-function bloqueioRenderizacaoAsync(){
-  setTimeout(bloqueioRenderizacao, 1000);
-}
